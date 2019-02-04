b688bf27955f4ffacb444df353353e78ed2f970c
Jonathan Dasheng Zhang <dz2276@tc.columbia.edu>
2019-02-04 08:42:22 -0500

    Added test input and styles

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    state = {
+        wordInput: ''
+    };
+
+    onChangeTextHandler = (val) => {
+        this.setState({
+            wordInput: val
+        })
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        placeholder="Enter a word you want to memorize"
+                        onChangeText={this.onChangeTextHandler}
+                        value={this.state.wordInput}
+                        style={{
+                            flex: 4
+                        }}/>
+                    <Button
+                        title="Add"
+                        onPress={() => {
+                        }}
+                        style={{
+                            flex: 1
+                        }}/>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 40
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        paddingHorizontal: 20
+    }
 });
